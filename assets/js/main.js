ec59622657db3ed76afc9dc25d4602e0b033059d
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-06-25 23:48:21 +0300

    Calculator Assignment 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,26 @@
-// *** Write your code here***
+// const calculator = () => {
+
+//   const clear = () => {  };
+
+//   const delete = () =>{};
+
+//   const appendNumber = (number) => {};
+//   const chooseOperation = (operation) => {};
+//   const compute = () => {};
+//   const updateDisplay = () => {};
+// };
+// calculator();
+
+let displayResult = document.querySelector('input[name="displayResult"]');
+
+const calcNumbers = (numValue) => {
+  document.querySelector('input[name="displayResult"]').value += numValue;
+
+  return numValue;
+};
+calcNumbers();
+
+const clear = () => {
+  document.querySelector('input[name="displayResult"]').value = "";
+};
+clear();
